0ed4eec9c95b068c644106e2440ae43eec341477
LuK1337 <priv.luk@gmail.com>
2025-08-09 17:52:11 +0500

    sdm845-common: Move media_codecs_c2.xml to device tree Change-Id: Ib2ebfff4281eafd46427180c2666a71d0d144300

--- sdm845.mk
+++ sdm845.mk
@@ -247,6 +247,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
