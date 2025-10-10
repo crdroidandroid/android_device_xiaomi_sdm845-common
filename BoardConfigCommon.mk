f74b7cbc18706e3edc45ee9ede0fb195328a48d1
duckyduckG <duckyduckg65@gmail.com>
2025-10-10 10:04:51 +0500

    test: drop framework_compatibility_matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,7 +67,6 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
